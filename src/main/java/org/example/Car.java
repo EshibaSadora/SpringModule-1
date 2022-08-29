c005e0412baf23c46929aeb2a05cf8d2e21005fa
EshibaSadora <egorshornikov@mail.ru>
2022-08-29 12:54:12 +0300

    Немного комментариев

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -3,7 +3,11 @@ package org.example;
 import java.util.UUID;
 
 public class Car {
+
+    //это уникальный номер автомобиля (добавил, потому что захотел)
     UUID Vin;
+
+    //Тип построенного автомобиля, что бы модели различать
     int Type;
 
     public Car(int type){
